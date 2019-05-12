48c2169a84a79c061f3cfede671372b0779ab966
Shrinath Nimare <shrinath.nimare@gmail.com>
2019-05-12 11:30:32 +0530

    Working on basic parsing framework

--- main.py
+++ main.py
@@ -17,7 +17,17 @@ THREAD_LIST_URL = 'http://a.4cdn.org/###/threads.json'
 #here we'll turn the JSON into actually usable lists of data
 #=================================================================================
 
-class DataAcquisitionClass:
+class CatalogParsingClass:
 	def __init__(self, board):
 		sess = requests.session()
-		catalog_json_data_from_4chin = sess.get(re.sub('###', board, CATALOG_URL)).text
+		raw_catalog_data = sess.get(re.sub('###', board, CATALOG_URL)).text
+		self._json_catalog_data = json.loads(raw_catalog_data)
+		self._parse_data()
+	
+	def _parse_data(self):
+		self._number_of_pages = len(self._json_catalog_data)
+		self._list_all_threads = []
+		for current_page in self._json_catalog_data:
+			for current_thread in range(len(current_page['threads'])):
+				self._list_all_threads.append(current_page[current_thread])
+		print(self._list_all_threads)
